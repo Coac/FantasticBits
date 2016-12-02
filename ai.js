57198a4fe7c62af859042b6b9a4f95f2016cc7d8
Coac <levictor3000@hotmail.fr>
2016-12-02 18:05:54 +0100

    var to let/const

--- ai.js
+++ ai.js
@@ -53,16 +53,16 @@ while (true) {
   var bludgers = [];
   var entities = [];
 
-  var entitiesCount = parseInt(readline()); // number of entities still in game
+  const entitiesCount = parseInt(readline()); // number of entities still in game
   for (let i = 0; i < entitiesCount; i++) {
-    var inputs = readline().split(' ');
-    var entityId = parseInt(inputs[0]); // entity identifier
-    var entityType = inputs[1]; // "WIZARD", "OPPONENT_WIZARD" or "SNAFFLE" (or "BLUDGER" after first league)
-    var x = parseInt(inputs[2]); // position
-    var y = parseInt(inputs[3]); // position
-    var vx = parseInt(inputs[4]); // velocity
-    var vy = parseInt(inputs[5]); // velocity
-    var state = parseInt(inputs[6]); // 1 if the wizard is holding a Snaffle, 0 otherwise
+    const inputs = readline().split(' ');
+    const entityId = parseInt(inputs[0]); // entity identifier
+    const entityType = inputs[1]; // "WIZARD", "OPPONENT_WIZARD" or "SNAFFLE" (or "BLUDGER" after first league)
+    const x = parseInt(inputs[2]); // position
+    const y = parseInt(inputs[3]); // position
+    const vx = parseInt(inputs[4]); // velocity
+    const vy = parseInt(inputs[5]); // velocity
+    const state = parseInt(inputs[6]); // 1 if the wizard is holding a Snaffle, 0 otherwise
 
     if (entityType === 'WIZARD') {
       let wizard = {
@@ -245,22 +245,22 @@ function interceptOnCircle (p1, p2, c, r) {
     // c is the circle's center
     // r is the circle's radius
 
-  var p3 = {x: p1.x - c.x, y: p1.y - c.y}; // shifted line points
-  var p4 = {x: p2.x - c.x, y: p2.y - c.y};
+  let p3 = {x: p1.x - c.x, y: p1.y - c.y}; // shifted line points
+  let p4 = {x: p2.x - c.x, y: p2.y - c.y};
 
-  var m = (p4.y - p3.y) / (p4.x - p3.x); // slope of the line
-  var b = p3.y - m * p3.x; // y-intercept of line
+  let m = (p4.y - p3.y) / (p4.x - p3.x); // slope of the line
+  let b = p3.y - m * p3.x; // y-intercept of line
 
-  var underRadical = Math.pow(r, 2) * Math.pow(m, 2) + Math.pow(r, 2) - Math.pow(b, 2); // the value under the square root sign
+  let underRadical = Math.pow(r, 2) * Math.pow(m, 2) + Math.pow(r, 2) - Math.pow(b, 2); // the value under the square root sign
 
   if (underRadical < 0) {
         // line completely missed
     return false;
   } else {
-    var t1 = (-m * b + Math.sqrt(underRadical)) / (Math.pow(m, 2) + 1); // one of the intercept x's
-    var t2 = (-m * b - Math.sqrt(underRadical)) / (Math.pow(m, 2) + 1); // other intercept's x
-    var i1 = {x: t1 + c.x, y: m * t1 + b + c.y}; // intercept point 1
-    var i2 = {x: t2 + c.x, y: m * t2 + b + c.y}; // intercept point 2
+    let t1 = (-m * b + Math.sqrt(underRadical)) / (Math.pow(m, 2) + 1); // one of the intercept x's
+    let t2 = (-m * b - Math.sqrt(underRadical)) / (Math.pow(m, 2) + 1); // other intercept's x
+    let i1 = {x: t1 + c.x, y: m * t1 + b + c.y}; // intercept point 1
+    let i2 = {x: t2 + c.x, y: m * t2 + b + c.y}; // intercept point 2
     return [i1, i2];
   }
 }
